1281c7222e8216df980b7decdee17b72a69853b8
palakbansal8810 <140965358+palakbansal8810@users.noreply.github.com>
2023-10-31 22:04:11 +0530

    Create game.py
    please accept this pr

---
+++ game.py
@@ -0,0 +1,27 @@
+import random
+
+def number_guessing_game():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    while True:
+        try:
+            user_guess = int(input("Guess the number: "))
+            attempts += 1
+
+            if user_guess < secret_number:
+                print("Too low! Try again.")
+            elif user_guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You've guessed the number {secret_number} in {attempts} attempts.")
+                break
+
+        except ValueError:
+            print("Please enter a valid number.")
+
+if __name__ == "__main__":
+    number_guessing_game()
